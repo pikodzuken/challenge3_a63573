b3e3417059c98b5c7f1277458f9a8863882598ac
Pi <ines.f.ribeiro12@gmail.com>
2026-03-12 14:55:07 +0000

    3.1 passo 3

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,11 +1,14 @@
+using System.Security;
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    private Rigidbody playerRb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.AddForce(Vector3.up * 1000);
     }
 
     // Update is called once per frame
